ebca52a1146b56aa63b6dd39061cbc7d87badeed
Banachowski Bartosz <brba@brba.local>
2019-01-22 10:32:45 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Banachowski Bartosz on 21/01/2019.
+//  Copyright © 2019 Banachowski Bartosz. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
